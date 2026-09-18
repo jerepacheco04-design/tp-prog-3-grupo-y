ce8ffe37b819a93e51e0509b82fe089af1d5dc3f
jere_ <jere_@JERE>
2026-09-17 23:44:53 -0300

    agregue prueba 1

--- tp-winform-equipo-Y/Dominio/Articulo.cs
+++ tp-winform-equipo-Y/Dominio/Articulo.cs
@@ -8,6 +8,7 @@ namespace Dominio
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
+        // prueba 1 
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public decimal Precio { get; set; }
