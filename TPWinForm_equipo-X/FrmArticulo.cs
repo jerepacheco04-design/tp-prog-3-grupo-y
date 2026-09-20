8152201925a6aca1d92038b146b4973e51899a8c
alanc <Alancaero57@gmail.com>
2026-09-19 21:59:25 -0300

    commit prueba

--- TPWinForm_equipo-X/FrmArticulo.cs
+++ TPWinForm_equipo-X/FrmArticulo.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using BusinessLogic;
+using Dominio;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using BusinessLogic;
-using Dominio;
 
 namespace TPWinForm_equipo_X
 {
